f386f49d4151342f813fcf6f98971d80f6800069
Igor Melo <igorjmelo4@gmail.com>
2019-11-06 00:00:05 -0300

    creating components and configuration

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class PostList extends Component {
+  state = {
+    newPost: '',
+    posts: [
+      {
+        id: 1,
+        author: {
+          name: "Julio Alcantara",
+          avatar: "http://url-da-imagem.com/imagem.jpg"
+        },
+        date: "04 Jun 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: "http://url-da-imagem.com/imagem.jpg"
+            },
+            content: "Conteúdo do comentário"
+          }
+        ]
+      },
+    ]
+  };
+
+  render() {
+    return <h1>PostList</h1>
+  }
+}
